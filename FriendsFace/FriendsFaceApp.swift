99b2c57f5afa7eccabdd62c3d820adae05e51e6c
Sergey Zakharov <zakharov.nsg@gmail.com>
2026-04-09 02:07:58 +0500

    Initial Commit

---
+++ FriendsFace/FriendsFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsFaceApp.swift
+//  FriendsFace
+//
+//  Created by Сергей Захаров on 09.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct FriendsFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
